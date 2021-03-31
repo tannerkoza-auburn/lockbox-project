c0d46064c7c24914aff8036fa5aad9f5420cb50c
Tanner Koza <kozatanner@gmail.com>
2021-03-31 09:02:16 -0900

    This adds code and necessary files. SPI-Py and facial_recognition are embedded repositories!

---
+++ state_machine.py
@@ -0,0 +1,88 @@
+import RPi.GPIO as GPIO
+import time
+from transitions import Machine, State
+
+
+
+class OurBox:
+
+    # Possible LockBox States
+    states = [
+        State(name='double_locked'),
+        State(name='single_locked'),
+        State(name='unlocked')
+    ]
+
+    # OurBox Instance Initialization
+    def __init__(self):
+        # Initialize the State Machine
+        self.machine = Machine(model=self, states=OurBox.states, initial='double_locked')
+
+        # Define Transitions
+        self.machine.add_transition('rfid_unlocking', 'double_locked', 'single_locked', after= 'rfid_unlock')
+        self.machine.add_transition('face_unlocking', 'single_locked', 'unlocked', after= 'face_unlock')
+        self.machine.add_transition('locking', 'unlocked', 'double_locked', after= 'manual_lock')
+        
+        # GPIO Setup
+        self.lockLED = 17
+        self.scanLED = 27
+        self.unlockLED = 22
+
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.lockLED, GPIO.OUT)
+        GPIO.setup(self.scanLED, GPIO.OUT)
+        GPIO.setup(self.unlockLED, GPIO.OUT)
+
+        # Lock OurBox Instance and begin RFID scan
+        self.auto_lock()
+
+
+    # Define Methods that control hardware
+
+    def rfid_unlock(self):
+            print('RFID SCAN...\n')
+            GPIO.output(self.scanLED, GPIO.HIGH)
+            time.sleep(10)
+            print('ACCESS GRANTED\n')
+            self.state
+            self.face_unlocking()       
+
+    def face_unlock(self):
+        print('FACE SCAN...\n')
+        future = time.time() + 10
+        while True:
+            if time.time() < future:
+                GPIO.output(self.scanLED, GPIO.HIGH)
+                time.sleep(0.5)
+                GPIO.output(self.scanLED, GPIO.LOW)
+                time.sleep(0.5)
+            else:
+                break
+            
+        GPIO.output(self.scanLED, GPIO.LOW)
+        GPIO.output(self.lockLED, GPIO.LOW)
+        GPIO.output(self.unlockLED, GPIO.HIGH)
+        print('ACCESS GRANTED\n')
+        self.state
+        self.locking()
+
+    def auto_lock(self):
+        print('LOCKED\n')
+        GPIO.output(self.unlockLED, GPIO.LOW)
+        GPIO.output(self.lockLED, GPIO.HIGH)
+        self.state
+        self.rfid_unlocking()
+        
+    def manual_lock(self):
+        lock_cmd = input('PRESS ENTER TO LOCK\n')
+        print('LOCKED\n')
+        GPIO.output(self.unlockLED, GPIO.LOW)
+        GPIO.output(self.lockLED, GPIO.HIGH)
+        self.state
+        self.rfid_unlocking()
+        
+try:
+    BOX = OurBox()
+    print('CTRL + C TO CANCEL/CLEAN')          
+except KeyboardInterrupt:
+    GPIO.cleanup()
